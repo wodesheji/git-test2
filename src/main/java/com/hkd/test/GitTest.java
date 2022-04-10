2d8148858a0d7a63588ade097e8c533f0b180761
shen <shen@qq.com>
2022-04-10 11:45:04 +0800

    master third commit

--- src/main/java/com/hkd/test/GitTest.java
+++ src/main/java/com/hkd/test/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("ssss");
         System.out.println("hot-fix");
+        System.out.println("master test");
     }
 }
