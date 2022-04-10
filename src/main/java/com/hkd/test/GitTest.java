a31873066a4d9cf6e75e218d53be66fa69cde069
wodesheji <100277741+wodesheji@users.noreply.github.com>
2022-04-10 15:34:38 +0800

    pull test

--- src/main/java/com/hkd/test/GitTest.java
+++ src/main/java/com/hkd/test/GitTest.java
@@ -2,7 +2,7 @@ package com.hkd.test;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("ssss2");
+        System.out.println("ssss3");
         System.out.println("hot-fix");
         System.out.println("master test");
         System.out.println("hot-fix test");
