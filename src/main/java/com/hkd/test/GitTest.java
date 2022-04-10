b14ae86fe56d751f4732f2fe65f0f6e19cfefd57
shen <shen@qq.com>
2022-04-10 11:38:46 +0800

    hot-fix first commit

--- src/main/java/com/hkd/test/GitTest.java
+++ src/main/java/com/hkd/test/GitTest.java
@@ -2,6 +2,7 @@ package com.hkd.test;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("ssss2");
+        System.out.println("ssss");
+        System.out.println("hot-fix");
     }
 }
